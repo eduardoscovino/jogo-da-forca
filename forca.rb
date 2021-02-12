070f209f1a752995f030e15f2ead0f4522ddcdd1
eduardoscovino <eduardo.scovino@gmail.com>
2021-02-12 09:02:49 -0300

    Update forca.rb
    Mostrando a palavra secreta em forma de "_" para o usuário

--- forca.rb
+++ forca.rb
@@ -1,2 +1,20 @@
-puts "Bem vindo ao jogo da forca!!"
+puts "****** Bem vindo ao jogo da forca!! ******"
 
+puts "Escolhendo a palavra secreta..."
+sleep(1)
+palavra_secreta = "pernambuco"
+view_palavra = palavra_secreta.chars.map { |letra| letra = "_" }.join(" ")
+
+puts "\n\n\nPronto, escolhida!\n\n\n"
+puts "Vamos começar! A palavra escolhida tem #{palavra_secreta.size} letras"
+puts "#{view_palavra}\n\n"
+
+# receber uma letra do usuário
+puts "Tentativa 1:"
+chute = gets.chomp
+# verificar se essa a palavra secreta contém essa letra
+puts palavra_secreta.include? chute
+# se sim:
+# substituir o _ pela letra
+# se não:
+# perdeu a rodada e passa para a próxima tentativa
