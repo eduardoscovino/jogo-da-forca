61ae1a762513d9f69ac80ee3a35dd4a754802441
eduardoscovino <eduardo.scovino@gmail.com>
2021-02-12 19:44:23 -0300

    Update forca.rb

--- forca.rb
+++ forca.rb
@@ -3,9 +3,11 @@ puts "****** Bem vindo ao jogo da forca!! ******"
 puts "Escolhendo a palavra secreta..."
 sleep(1)
 palavra_secreta = "pernambuco"
+array_palavra = palavra_secreta.chars
 view_palavra = palavra_secreta.chars.map { |letra| letra = "_" }.join(" ")
 
 puts "\n\n\nPronto, escolhida!\n\n\n"
+sleep(1)
 puts "Vamos começar! A palavra escolhida tem #{palavra_secreta.size} letras"
 puts "#{view_palavra}\n\n"
 
@@ -13,8 +15,17 @@ puts "#{view_palavra}\n\n"
 puts "Tentativa 1:"
 chute = gets.chomp
 # verificar se essa a palavra secreta contém essa letra
-puts palavra_secreta.include? chute
+def tem_a_letra?(palavra, letra)
+  array_palavra.include? letra
+end
+puts tem_a_letra?(palavra_secreta, chute)
 # se sim:
 # substituir o _ pela letra
 # se não:
 # perdeu a rodada e passa para a próxima tentativa
+
+if tem_a_letra?(palavra, letra)
+  palavra.find_index(letra)
+else
+  puts "Errou!"
+end
