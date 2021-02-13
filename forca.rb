81d399ae91f6f2827dc774b100db82461e8c67ce
eduardoscovino <eduardo.scovino@gmail.com>
2021-02-13 17:45:01 -0300

    teste1

--- forca.rb
+++ forca.rb
@@ -1,4 +1,18 @@
-require_relative 'ui'
+def boas_vindas
+  puts "****** Bem vindo ao jogo da forca!! ******"
+end
+
+def avisa_escolha
+  puts "Escolhendo a palavra secreta..."
+  sleep(1)
+  puts "\n\nPronto! Palavra escolhida!"
+end
+
+def diz_num_letras(palavra_secreta, view)
+  sleep(1)
+  puts "Vamos começar! A palavra escolhida tem #{palavra_secreta.size} letras"
+  puts "\n\n#{view.join(" ")}\n\n"
+end
 
 def tem_a_letra?(palavra, chute)
   palavra.include? chute
