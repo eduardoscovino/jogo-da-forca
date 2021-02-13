19ba6ebaf90e81ee54d0715cce09ec3d64d65d64
eduardoscovino <eduardo.scovino@gmail.com>
2021-02-13 08:19:15 -0300

    Update forca.rb
    Agora já substitui letras repétidas na palavra secreta

--- forca.rb
+++ forca.rb
@@ -1,31 +1,35 @@
+def tem_a_letra?(palavra, chute)
+  palavra.include? chute
+end
+
+def substitui_chute_certo(palavra, chute, view)
+  palavra.each_with_index do |letra, index|
+    letra == chute ? view[index] = letra : view[index]
+    view
+  end
+end
+
 puts "****** Bem vindo ao jogo da forca!! ******"
 
 puts "Escolhendo a palavra secreta..."
 sleep(1)
-palavra_secreta = "pernambuco"
+palavra_secreta = "palavra"
 array_palavra = palavra_secreta.chars
-view_palavra = palavra_secreta.chars.map { |letra| letra = "_" }.join(" ")
+view_palavra = ['_', '_', '_', '_', '_', '_', '_']
 
 puts "\n\n\nPronto, escolhida!\n\n\n"
 sleep(1)
 puts "Vamos começar! A palavra escolhida tem #{palavra_secreta.size} letras"
 puts "#{view_palavra}\n\n"
 
-# receber uma letra do usuário
 puts "Tentativa 1:"
 chute = gets.chomp
-# verificar se essa a palavra secreta contém essa letra
-def tem_a_letra?(palavra, letra)
-  array_palavra.include? letra
-end
-puts tem_a_letra?(palavra_secreta, chute)
-# se sim:
-# substituir o _ pela letra
-# se não:
-# perdeu a rodada e passa para a próxima tentativa
 
-if tem_a_letra?(palavra, letra)
-  palavra.find_index(letra)
+if tem_a_letra?(array_palavra, chute)
+  puts "Acertou!"
+  substitui_chute_certo(array_palavra, chute, view_palavra)
+  p "#{view_palavra.join(" ")}"
 else
   puts "Errou!"
 end
+
