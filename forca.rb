461e52b96b8aec6fe255dde4af24556fa03cfa42
eduardoscovino <eduardo.scovino@gmail.com>
2021-02-13 18:05:19 -0300

    inserido o while com alteração do array de "_" e adição de 1 tentativa quando o jogador erra

--- forca.rb
+++ forca.rb
@@ -14,8 +14,30 @@ def diz_num_letras(palavra_secreta, view)
   puts "\n\n#{view.join(" ")}\n\n"
 end
 
+def avisa_tentativa(tentativa, maximo)
+  puts "Tentativa #{tentativa} de #{maximo} possíveis"
+end
+
+def pede_chute
+  puts "Qual é o seu chute?"
+end
+
+def avisa_chute(chute)
+  puts "Seu chute foi #{chute}"
+end
+
 def tem_a_letra?(palavra, chute)
   palavra.include? chute
+end
+
+def acertou_chute(array, chute, view)
+  puts "Acertou!"
+  substitui_chute_certo(array, chute, view)
+  puts "\n#{view.join(" ")}"
+end
+
+def errou_chute
+  puts "Errou!"
 end
 
 def substitui_chute_certo(palavra, chute, view)
@@ -25,22 +47,34 @@ def substitui_chute_certo(palavra, chute, view)
   end
 end
 
+# dando boas vindas
 boas_vindas
 avisa_escolha
+
+# escolhendo a palavra
 palavra_secreta = "palavra"
 array_palavra = palavra_secreta.chars
 view_palavra = ['_', '_', '_', '_', '_', '_', '_']
 diz_num_letras(palavra_secreta, view_palavra)
 
-puts "Tentativa 1:"
-chute = gets.chomp
-puts "Seu chute foi #{chute}"
+# setando as tentativas
+max_tentativas = 5
+tentativa_atual = 1
 
-if tem_a_letra?(array_palavra, chute)
-  puts "Acertou!"
-  substitui_chute_certo(array_palavra, chute, view_palavra)
-  puts "\n#{view_palavra.join(" ")}"
-else
-  puts "Errou!"
+while tentativa_atual <= max_tentativas
+  # num de tentativas
+  avisa_tentativa(tentativa_atual, max_tentativas)
+
+  # recebendo o chute
+  pede_chute
+  chute = gets.chomp
+  avisa_chute(chute)
+
+  if tem_a_letra?(array_palavra, chute)
+    acertou_chute(array_palavra, chute, view_palavra)
+  else
+    errou_chute
+    tentativa_atual += 1
+  end
 end
 
